840342473ab72ce18c9e0f34505aa145853b3b24
DaNiLoStOrM <storm.danilo@gmail.com>
2026-08-05 21:03:17 -0300

    Adiciona pacote principal

---
+++ umbot/__init__.py
@@ -0,0 +1,10 @@
+from .core import CharacterEntry, EngineDetector, GameProfile, StageEntry
+from .scanner import RosterScanner
+
+__all__ = [
+    "CharacterEntry",
+    "EngineDetector",
+    "GameProfile",
+    "RosterScanner",
+    "StageEntry",
+]
