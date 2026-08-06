39c599c384f90df6d2960638bc86daaa96c71ad8
DaNiLoStOrM <storm.danilo@gmail.com>
2026-08-05 21:03:09 -0300

    Adiciona ponto de entrada do aplicativo

---
+++ universal_mugen_bot.py
@@ -0,0 +1,14 @@
+from umbot import CharacterEntry, EngineDetector, GameProfile, RosterScanner, StageEntry
+from umbot.ui import main
+
+__all__ = [
+    "CharacterEntry",
+    "EngineDetector",
+    "GameProfile",
+    "RosterScanner",
+    "StageEntry",
+    "main",
+]
+
+if __name__ == "__main__":
+    main()
